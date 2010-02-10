66187de59327f29d35c00f0462e94d4100204ae5
Mark Grimes <mgrimes@peculier.com>
2010-02-10 16:31:18 -0500

    Added some basic pod documentation

--- lib/Plack/Middleware/AutoRefresh.pm
+++ lib/Plack/Middleware/AutoRefresh.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use parent qw( Plack::Middleware );
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 use Plack::Util;
 use Plack::Util::Accessor qw( dirs filter wait );
@@ -154,3 +154,105 @@ sub _get_script {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Plack::Middleware::AutoRefresh - Reload pages in browsers when files are modified
+
+=head1 SYNOPSIS
+
+    # in app.psgi
+    use Plack::Builder;
+
+    builder {
+        enable 'Plack::Middleware::AutoRefresh',
+               dirs => [ qw/html/ ], filter => qr/\.(swp|bak)/;
+        $app;
+    }
+
+=head1 DESCRIPTION
+
+Plack::Middleware::AutoRefresh is a middleware component that will reload
+you web pages in your browser when changes are detected in the source
+files. It should work with any modern browser that supports JavaScript and
+multiple browsers simultaneously.
+
+=head1 CONFIGURATION
+
+    dirs => [ '.' ]                     # default
+    dirs => [ qw/root share html/ ]
+
+Specifies the directories to watch for changes. Will watch all files 
+and subdirectories of the specified directories for file modifications,
+new files, deleted files, new directories and deleted directories.
+
+    filter => qr/\.(swp|bak)$/           # default
+    filter => qr/\.(svn|git)$/
+    filter => sub { shift =~ /\.html$/ }
+    
+Will apply the specified filter to the changed path name. This can be
+a regular expression or a code ref. Any paths that match the regular
+expression will be ignored. A code ref will be passed the path as the
+only argument. Any false return values will be filtered out.
+
+    wait => 5                           # default 
+
+Wait indicated the maximum number of seconds that the client should
+block for while waiting for notifications of changes. Setting this to
+a lower value will I<not> improve response times. 
+
+=head1 ACKNOWLEDGMENTS
+
+This component was inspired by NodeJuice (L<http://nodeJuice.com/>).
+NodeJuice provides very similar browser refresh functionality by
+running a standalone proxy between your client and the web
+application. It is a bit more robust than
+Plack::Middleware::AutoRefresh as it can handle critical errors in
+your app (ie, compile errors).  Plack::Middleware::AutoRefresh is
+simpler to setup and is limited to L<Plack> based applications. Some
+of the original JavaScript was taken from nodeJuice project as well,
+although it was mostly rewritten prior to release. Thank you to
+Stephen Blum the author of nodeJuice.
+
+A huge thank you to the man behind L<Plack>, Tatsuhiko Miyagawa, who
+help me brainstorm the implementation, explained the inners of the
+Plack servers, and re-wrote my broken code. 
+
+=head1 IMPLEMENTATION
+
+Plack::Middleware::AutoRefresh accomplishes the browser refresh by
+inserting a bit (1.2K to be precise) of JavaScript into the (x)html
+pages your Plack application on the fly. The JavaScript tries to have
+minimal impact: only one anonymous function and one global flag
+(window['-plackAutoRefresh-']) are added. The JavaScript will open an
+Ajax connection back to your Plack server which will block waiting to
+be notified of changes. When a change notification arrives, the
+JavaScript will trigger a page reload.
+
+=head1 SEE ALSO
+
+NodeJuice at L<http://www.nodejuice.com/>.
+
+Modules used to implement this module L<AnyEvent::Filesys::Notify>. 
+
+And of course, L<Plack>.
+
+=head1 BUGS
+
+Please report any bugs or suggestions at L<http://rt.cpan.org/>
+
+=head1 AUTHOR
+
+Mark Grimes, E<lt>mgrimes@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2010 by Mark Grimes
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
